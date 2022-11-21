b02ecb7a71a401af288eaf521ca5cbc856ecb825
Momchil Tsekov <mtsekov@zohomail.eu>
2022-11-21 21:20:31 +0200

    webpack config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,57 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-let mode = "development";
+let mode = 'development'
 
-process.env.NODE_ENV === "production"
-	? (mode = "production")
-	: (mode = "development");
+process.env.NODE_ENV === 'production'
+  ? (mode = 'production')
+  : (mode = 'development')
 
 module.exports = {
-	mode: mode,
+  mode: mode,
 
-	module: {
-		rules: [
-			{
-				test: /\.(s[ac]|c)ss$/i,
-				use: [
-					MiniCssExtractPlugin.loader,
-					"css-loader",
-					"postcss-loader",
-					"sass-loader",
-				],
-			},
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-				},
-			},
-		],
-	},
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'assets/resource',
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '',
+            },
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
 
-	plugins: [new MiniCssExtractPlugin()],
+  plugins: [new MiniCssExtractPlugin()],
 
-	resolve: {
-		extensions: [".js", ".jsx"],
-	},
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 
-	devtool: "source-map",
-	devServer: {
-		static: {
-			directory: path.join(__dirname, "dist"),
-		},
-		// Default is true
-		/* hot: true, */
-	},
-};
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    // Default is true
+    /* hot: true, */
+  },
+}
